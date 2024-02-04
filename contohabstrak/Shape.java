54a791d3b04f1590dcf66aed6c1ffd7fcab9cd42
VigoCihuyy <136456279+SkyZone-Ai@users.noreply.github.com>
2024-02-04 18:32:51 +0700

    Add files via upload

---
+++ contohabstrak/Shape.java
@@ -0,0 +1,17 @@
+
+package contohabstrak;
+
+
+public abstract class Shape {
+
+   String = color;
+   
+   void setColor(String color){
+       this.color = color;
+   }
+   String get.Shape(){
+       return this.color;
+   }
+    
+   abstract float getArea();
+}
